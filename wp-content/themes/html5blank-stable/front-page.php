8d795a355392c0f9553610d2bee48d905660f2b3
jpichon21 <juliencfpichon@gmail.com>
2020-03-27 13:13:09 +0100

    [ ALL ] Clean head + add analytics

--- wp-content/themes/html5blank-stable/front-page.php
+++ wp-content/themes/html5blank-stable/front-page.php
@@ -1,39 +1,4 @@
 <?php get_header(); ?>
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!-->
-<html class="no-js">
-<!--<![endif]-->
-<!---------------------------------------->
-<!---------------------------------------->
-<!--- DEVELOPPEMENT PAR NCP MULTIMEDIA --->
-<!------https://ncpmultimedia.com/-------->
-<!---------------------------------------->
-<!---------------------------------------->
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-    <title>Horizon | Agence de Consulting </title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="stylesheet" href="https://use.typekit.net/zuv7hqs.css">
-    <link rel="stylesheet" src="main.css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Montserrat:200,200i,300,300i,400,400i,500,500i,600,600i&display=swap"
-        rel="stylesheet">
-
-    <link rel="stylesheet" src="<?php echo get_template_directory_uri() . '/spectre.min.css'; ?>">
-    <link rel="stylesheet" src="<?php echo get_template_directory_uri() . '/slick.css'; ?>">
-    <link rel="stylesheet" src="<?php echo get_template_directory_uri() . '/slick-theme.css'; ?>">
-
-    <script src="<?php echo get_template_directory_uri() . '/js/jquery-3.4.1.min.js'; ?>"></script>
-    <script src="<?php echo get_template_directory_uri() . '/js/slick.min.js'; ?>"></script>
-</head>
-
 <body>
     <div id="mainContainer" class="container">
 
--- wp-content/themes/html5blank-stable/header.php
+++ wp-content/themes/html5blank-stable/header.php
@@ -1,15 +1,48 @@
 <!doctype html>
 <html <?php language_attributes(); ?> class="no-js">
 	<head>
+	<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+	<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+	<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+	<!--[if gt IE 8]><!-->
+	<!--<![endif]-->
+	<!---------------------------------------->
+	<!---------------------------------------->
+	<!--- DEVELOPPEMENT PAR NCP MULTIMEDIA --->
+	<!------https://ncpmultimedia.com/-------->
+	<!---------------------------------------->
+	<!---------------------------------------->
+
+
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-162007895-1"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+			gtag('config', 'UA-162007895-1');
+		</script>
+		<!-- -->
+
+
 		<meta charset="<?php bloginfo('charset'); ?>">
 		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
 
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+		<link rel="stylesheet" href="https://use.typekit.net/zuv7hqs.css">
+    	<link rel="stylesheet" src="main.css">
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:200,200i,300,300i,400,400i,500,500i,600,600i&display=swap" rel="stylesheet">
+		<link rel="stylesheet" src="<?php echo get_template_directory_uri() . '/spectre.min.css'; ?>">
+		<link rel="stylesheet" src="<?php echo get_template_directory_uri() . '/slick.css'; ?>">
+		<link rel="stylesheet" src="<?php echo get_template_directory_uri() . '/slick-theme.css'; ?>">
+
+		<script src="<?php echo get_template_directory_uri() . '/js/jquery-3.4.1.min.js'; ?>"></script>
+		<script src="<?php echo get_template_directory_uri() . '/js/slick.min.js'; ?>"></script>
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
 
 		<?php wp_head(); ?>
